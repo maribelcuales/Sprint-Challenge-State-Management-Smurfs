5c0e5eac1f7cc088b659b40c8bef718c02bcb214
maribelcuales <maribelcuales@gmail.com>
2020-07-25 05:33:16 +0800

    Create Redux store and pass it as prop value to Provider.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux'; 
+import thunk from 'redux-thunk';   
+
 import "./index.css";
 
 import App from "./components/App";
+import rootReducer from './store/reducers'; 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById("root"));
