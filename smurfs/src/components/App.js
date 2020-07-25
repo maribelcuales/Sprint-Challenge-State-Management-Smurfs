42b5507f7f5a35df7877bc78e19991881930d91d
maribelcuales <maribelcuales@gmail.com>
2020-07-25 16:17:01 +0800

    Added smurfForm component.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 
 import SmurfsList from "./SmurfsList";
+import SmurfForm from "./SmurfForm"; 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SmurfForm />
         <SmurfsList /> 
 
         {/* <h1>SMURFS! 2.0 W/ Redux</h1>
