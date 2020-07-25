c4c929180945839b16738ccbae937a2b224e7877
maribelcuales <maribelcuales@gmail.com>
2020-07-25 10:10:01 +0800

    Created smurfActions and fetchSmurf function.

---
+++ smurfs/src/store/actions/smurfActions.js
@@ -0,0 +1,19 @@
+import axios from 'axios'; 
+
+export const FETCH_SMURF_FAILURE = 'FETCH_SMURF_FAILURE';
+
+export const fetchSmurf = () => {
+  return dispatch => {
+    dispatch({ type: 'FETCH_SMURF_START' }); 
+    axios
+      .get('http://localhost:3333/smurfs')
+      .then(response => {
+        console.log(response); // response.data
+        dispatch({ type: 'FETCH_SMURF_SUCCESS', payload: response.data})
+      })
+      .catch(error => {
+        console.log({error});
+        dispatch({ type: 'FETCH_SMURF_FAILURE', payload: `${error}`})
+      });
+  };
+};
