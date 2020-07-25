e5f4affc92814061e342b6c87169db04dd3b6446
maribelcuales <maribelcuales@gmail.com>
2020-07-25 12:18:52 +0800

    Added case logic for fetching smurfs in smurfsReducer.

--- smurfs/src/store/reducers/smurfsReducer.js
+++ smurfs/src/store/reducers/smurfsReducer.js
@@ -6,6 +6,24 @@ const initialState = {
 
 export const smurfsReducer = (state=initialState , action) => {
   switch (action.type) {
+    case "FETCH_SMURF_START": 
+      return {
+        ...state, 
+        isFetching: true
+      };
+    case "FETCH_SMURF_SUCCESS": 
+      return {
+        ...state,
+        isFetching: false,
+        smurfs: [...state.smurfs, action.payload],
+        error: ""
+      };
+    case "FETCH_FILM_FAILUIRE": 
+      return {
+        ...state,
+        isFetching: false,
+        error: action.payload
+      }
     default: 
     return state;
   }
