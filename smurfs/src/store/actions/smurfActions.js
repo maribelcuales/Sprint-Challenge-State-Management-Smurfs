7348ed5eb246d7638d8debb24ee05c48747bebbd
maribelcuales <maribelcuales@gmail.com>
2020-07-25 20:04:45 +0800

    Refactored addNewSmurf action.

--- smurfs/src/store/actions/smurfActions.js
+++ smurfs/src/store/actions/smurfActions.js
@@ -18,6 +18,29 @@ export const fetchSmurf = () => {
   };
 };
 
-export const addSmurf = newSmurf => {
-  return { type: "ADD_NEW_SMURF", payload: newSmurf }
-}
+export const addNewSmurf = newSmurf => {
+  return dispatch => {
+    // dispatch({ type: "ADD_NEW_SMURF", payload: newSmurf}); 
+    axios
+      .post('http://localhost:3333/smurfs/', {
+        name: newSmurf.name,
+        age: newSmurf.age,
+        height: newSmurf.height,
+        id: Date.now()      
+      })
+      .then(response => {
+        console.log('post new smurf', response); 
+      })
+      .catch(error => {
+        console.log({error});
+        dispatch({ type: "ADD_SMURF_FAILURE", payload: `${error}` })
+      });
+  };
+}; 
+
+// export const getSmurfs = () => {
+//   return { type: "GET_ALL_SMURFS" }; 
+// }
+
+
+
